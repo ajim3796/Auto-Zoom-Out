68003deec6c0292fa972e5471fabca7d0b198786
ajim <n1a1o2k0i@gmail.com>
2024-11-11 03:40:13 +0900

    update 1.0.0

--- content.js
+++ content.js
@@ -10,26 +10,50 @@ let sWidth;
 let sLeft;
 let type;
 let size;
-let tmp;
+
+const fit = () => {
+  if (type === "zoom") {
+    document.body.style.zoom = size + "%";
+  } else if (type === "width") {
+    document.body.style.width = size + "%";
+  }
+};
+
+const getItems = () => {
+  width = window.innerWidth;
+  urlWidth = url + " " + width;
+  hostWidth = host + " " + width;
+  chrome.storage.local.get()
+    .then((items) => {
+      if (items[url]) {
+        type = items[url][0];
+        size = items[url][1];
+      } else if (items[host]) {
+        type = items[host][0];
+        size = items[host][1];
+      } else if (items[urlWidth]) {
+        type = items[urlWidth][0];
+        size = items[urlWidth][1];
+      } else if (items[hostWidth]) {
+        type = items[hostWidth][0];
+        size = items[hostWidth][1];
+      } else {
+        type = "zoom";
+        size = "100";
+      }
+    })
+    .then(fit)
+    .catch(e => { console.error(e); });
+}
 
 const scrollCheck = () => {
   wHeight = window.innerHeight;
   cHeight = document.documentElement.clientHeight;
-  document.body.style.zoom = "";
-  document.body.style.width = "";
-  if (wHeight - cHeight === 0) {
-    type = "no";
+  if (wHeight === cHeight) {
+    type = "zoom";
     size = "100";
   } else {
     sizeCheck();
-  }
-};
-
-const fit = (type, size) => {
-  if (type == "zoom") {
-    document.body.style.zoom = size + "%";
-  } else if (type == "width") {
-    document.body.style.width = size + "%";
   }
 };
 
@@ -41,7 +65,7 @@ const sizeCheck = () => {
   } else if (document.documentElement.scrollLeft != 0) {
     sLeft = document.documentElement.scrollLeft;
   }
-  if (cWidth / sWidth === 1) {
+  if (cWidth === sWidth) {
     size = Math.floor((1 - sLeft / cWidth) * 100 - 1).toString();
   } else {
     size = Math.floor((cWidth / sWidth) * 100 - 1).toString();
@@ -53,9 +77,9 @@ const zoomFit = () => {
   document.body.style.zoom = size + "%";
   wHeight = window.innerHeight;
   cHeight = document.documentElement.clientHeight;
-  if (wHeight - cHeight === 0) {
+  if (wHeight === cHeight) {
     type = "zoom";
-    setHost(type, size);
+    setHost();
   } else {
     document.body.style.zoom = "";
     widthFit();
@@ -66,146 +90,74 @@ const widthFit = () => {
   document.body.style.width = size + "%";
   wHeight = window.innerHeight;
   cHeight = document.documentElement.clientHeight;
-  if (wHeight - cHeight === 0) {
+  if (wHeight === cHeight) {
     type = "width";
-    setHost(type, size);
+    setHost();
   } else {
     document.body.style.width = "";
-    type = "no";
+    type = "zoom";
     size = "100";
     document.body.style.overflowX = "hidden";
     document.documentElement.style.overflowX = "hidden";
   }
 };
 
-const setHost = (type, size) => {
+const setHost = () => {
   reset();
   empty = {};
   empty[hostWidth] = [type, size];
   chrome.storage.local.set(empty);
 };
 
+const setUrl = () => {
+  reset();
+  empty = {};
+  empty[urlWidth] = [type, size];
+  chrome.storage.local.set(empty);
+};
+
 const setNoChange = (key) => {
   reset();
-  type = "no";
+  type = "zoom";
   size = "100";
   empty = {};
   empty[key] = [type, size];
   chrome.storage.local.set(empty);
-  document.body.style.zoom = "";
-  document.body.style.width = "";
 };
 
 const reset = () => {
-  chrome.storage.local.remove(url);
-  chrome.storage.local.remove(host);
-  chrome.storage.local.remove(urlWidth);
-  chrome.storage.local.remove(hostWidth);
+  document.body.style.zoom = "";
+  document.body.style.width = "";
+  chrome.storage.local.remove(url).catch(e => { console.error(e); });
+  chrome.storage.local.remove(host).catch(e => { console.error(e); });
+  chrome.storage.local.remove(urlWidth).catch(e => { console.error(e); });
+  chrome.storage.local.remove(hostWidth).catch(e => { console.error(e); });
 };
 
-const noScrollbar = () => {
-  var newStyle = document.createElement("style");
-  newStyle.type = "text/css";
-  newStyle.innerText = "::-webkit-scrollbar {display: none;}";
-  document.getElementsByTagName("HEAD").item(0).appendChild(newStyle);
-};
+getItems();
 
-(function () {
-  width = window.innerWidth;
-  urlWidth = url + " " + width;
-  hostWidth = host + " " + width;
-  chrome.storage.local.get(function (items) {
-    if (items[url]) {
-      type = items[url][0];
-      size = items[url][1];
-      tmp = "exist";
-    } else if (items[host]) {
-      type = items[host][0];
-      size = items[host][1];
-      tmp = "exist";
-    } else if (items[urlWidth]) {
-      type = items[urlWidth][0];
-      size = items[urlWidth][1];
-      tmp = "exist";
-    } else if (items[hostWidth]) {
-      type = items[hostWidth][0];
-      size = items[hostWidth][1];
-      tmp = "exist";
-    } else {
-      tmp = "not exist";
-    }
-    if (tmp === "exist") {
-      window.addEventListener("DOMContentLoaded", function () {
-        fit(type, size);
-      });
-    } else {
-      window.addEventListener("load", scrollCheck);
-    }
-  });
-})();
+window.addEventListener('resize', getItems);
 
-window.addEventListener(
-  "resize",
-  function () {
-    width = window.innerWidth;
-    urlWidth = url + " " + width;
-    hostWidth = host + " " + width;
-    chrome.storage.local.get(function (items) {
-      if (items[url]) {
-        type = items[url][0];
-        size = items[url][1];
-        tmp = "exist";
-      } else if (items[host]) {
-        type = items[host][0];
-        size = items[host][1];
-        tmp = "exist";
-      } else if (items[urlWidth]) {
-        type = items[urlWidth][0];
-        size = items[urlWidth][1];
-        tmp = "exist";
-      } else if (items[hostWidth]) {
-        type = items[hostWidth][0];
-        size = items[hostWidth][1];
-        tmp = "exist";
-      } else {
-        tmp = "not exist";
-      }
-      if (tmp === "exist") {
-        fit(type, size);
-      } else {
-        scrollCheck();
-      }
-    });
-  },
-  false
-);
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request === "popup") {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.msg === "popup") {
     sendResponse([type, size]);
-  } else if (request === "save_domain") {
-    setNoChange(host);
-  } else if (request === "save_url") {
-    setNoChange(url);
-  } else if (request === "reset") {
+  } else if (request.msg === "check") {
+    scrollCheck();
+    sendResponse([type, size]);
+  } else if (request.msg === "save_host") {
+    type = request.type;
+    size = request.size;
+    setHost();
+    fit();
+  } else if (request.msg === "save_url") {
+    type = request.type;
+    size = request.size;
+    setUrl();
+    fit();
+  } else if (request.msg === "reset") {
+    type = "zoom";
+    size = "100";
     reset();
-    location.reload();
-  } else if (request === "no scrollbar") {
-    noScrollbar();
-  } else if (request === "no change") {
-    setNoChange(hostWidth);
-  } else if (Array.isArray(request)) {
-    type = request[0];
-    size = request[1];
-    if (type === "zoom") {
-      document.body.style.zoom = size + "%";
-      document.body.style.width = "";
-      setHost(type, size);
-    } else if (type === "width") {
-      document.body.style.width = size + "%";
-      document.body.style.zoom = "";
-      setHost(type, size);
-    }
   }
   return true;
 });
